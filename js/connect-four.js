95b53a82c91793e6df93591deec60d68b01b250d
Louis Chatriot <louis.chatriot@gmail.com>
2017-08-17 07:48:50 -0700

    Purely console

--- js/connect-four.js
+++ js/connect-four.js
@@ -1,8 +1,12 @@
 // Barebones Connect Four
 
 var rows = 6, columns = 7
+  , topRows = 2   // Number of virtual rows on top of the grid that will be used for animation
   , board   // 0 is empty, -1 is red and 1 is yellow
   , currentPlayer
+  , width, height
+  , cellSize   // Cells are square
+  , $svg = $("body svg")
   ;
 
 
@@ -19,6 +23,31 @@ function init () {
   }
 
   currentPlayer = 1;   // Yellow starts
+
+  // Calculate dimensions for drawing (use the largest viewport that can accomodate full grid and top rows)
+  var wWidth = $(window).width(), wHeight = $(window).height()
+    , cellSize = Math.min(wWidth / columns, wHeight / (rows + topRows))
+    , containerWidth = cellSize * columns
+    , containerHeight = cellSize * (rows + topRows)
+    ;
+
+  // Draw grid and lines
+  return;
+  $svg.find("").remove();
+  //$svg.attr("width", containerWidth);
+  //$svg.attr("height", containerHeight);
+  return;
+  $svg.append('<line style="stroke: rgb(255,0,0); stroke-width: 2;" x1="10" x2="10" y1="50" y2="50"></line>');
+  var $line;
+    $line = $('<line style="stroke: rgb(255,0,0); stroke-width: 2;">');
+    $line.attr("x1","100");
+    $line.attr("x2","100");
+    $line.attr("y1","500");
+    $line.attr("y2","500");
+    $svg.append($line);
+    console.log($line);
+  for (var j = 0; j <= rows; j += 1) {
+  }
 }
 
 // If player is not supplied, alternate
